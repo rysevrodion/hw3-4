7c21cc266eecaee1911cb62ae1f31141eb4a8f78
rysevrodion <119106517+rysevrodion@users.noreply.github.com>
2022-12-04 05:47:18 +0700

    Update 4index.php

--- hw4/4index.php
+++ hw4/4index.php
@@ -1,11 +1,20 @@
 <?php
 require '4structure.php';
-
+echo "Тариф Базовый 5км 59 минут, доп. услуги GPS и Водитель: ";
 $tariff = new TariffHour(5, 59);
-//$tariff = new TariffStudent(5,20);
-//$tariff = new TariffBasic(5,20);
 $tariff->add(new additionGPS());
 $tariff->add(new additionDriver());
-echo $tariff->calcPrice();
+echo $tariff->calcPrice() . "<br>" . "<br>";
+
+
+echo "Тариф Студенческий 7км 40 минут, доп. услуга GPS: ";
+$tariff = new TariffStudent(7,40);
+$tariff->add(new additionGPS());
+echo $tariff->calcPrice() . "<br>" . "<br>";
+
+
+echo "Тариф Почасовой 5км 89 минут: ";
+$tariff = new TariffHour(5,20);
+echo $tariff->calcPrice() . "<br>" . "<br>";
 
 ?>
